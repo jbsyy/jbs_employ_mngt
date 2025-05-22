c22a4996443852259cbf8abf97e999d7be0f25cb
jubs <mjubainne@gmail.com>
2025-05-22 13:12:42 +0800

    fix me

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -9,38 +9,39 @@ use App\Models\employee;
 class employeecontroller extends Controller
 {
     public function index()
-    {   
-        $employees = employee::get();
-
-        return view ('employee.index', compact('employees'));
-    }
-
-    public function create()
     {
-        return view ('employee.create');
+
+        return view ('employee.index');
     }
+
+    // public function create()
+    // {
+    //     $employees = employee::all();
+    //     return view ('employee.create');
+    // }
 
 
     public function store(Request $request)
     {
     $request->validate([
         'fname' => 'required|max:255|string',
-        'lname' => 'required|max:255|string',
+        'lname' => 'required|max:255|integer',
         'midname' => 'required|max:255|string',
         'age' => 'required|integer',
         'address' => 'required|max:255|string',
-        'zip' => 'required|integer',
-        
+        'zip' => 'required|date',
+        'date' => 'datestamps'
+
     ]);
 
-    employee::create($request->all());
+    employee::show($request->all());
     return view ('employee.create');
     }
 
     public function edit( int $id)
     {
         $employees = employee::find($id);
-        return view ('employee.edit',compact('employees'));
+        return view ('employee.edit');
     }
 
     public function update(Request $request, int $id) {
@@ -52,9 +53,10 @@ class employeecontroller extends Controller
                 'age' => 'required|integer',
                 'address' => 'required|max:255|string',
                 'zip' => 'required|integer',
-                
+                'date' => 'datestamps',
+
             ]);
-        
+
             employee::findOrFail($id)->update($request->all());
             return redirect ()->back()->with('status','Employee Updated Successfully!');
             }
@@ -62,7 +64,7 @@ class employeecontroller extends Controller
 
     public function destroy(int $id){
         $employees = employee::findOrFail($id);
-        $employees->delete();
+        $employees->destroy();
         return redirect ()->back()->with('status','Employee Deleted');
     }
 }
--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -19,14 +19,14 @@
         <a href="{{ route('employee.create') }}" class="btn btn-info">Add New Employee</a>
         <div class="row">
 
-       
+
                 <div class="card-head">
-                     
+
                 </div>
 
                 <div class="card-body">
 
-                    
+
 
                     <table class="table table-bordered table-stiped fs-1 text-black">
 
@@ -43,36 +43,29 @@
                         </thead>
 
                         <tbody  >
-                            @foreach ($employees as $items)
                             <tr>
-                                
-                                <td class="">{{$items->id}}</td>
-                                <td>{{$items->fname}}</td>
-                                <td>{{$items->lname}}</td>
-                                <td>{{$items->midname}}</td>
-                                <td>{{$items->age}}</td>
-                                <td>{{$items->address}}</td>
-                                <td>{{$items->zip}}</td>
-                                <td> 
-                                    <span class="badge bg-success"><a href="{{  route('employee.edit',$items->id)}}" class="btn btn-success mx-3  "><h5>Edit</h5></a></span>
+
+                                <td class=""></td>
+                                <td>
+                                    <span class="badge bg-success"><a href="{{  route('employee.edit')}}" class="btn btn-success mx-3  "><h5>Edit</h5></a></span>
                                 </td>
-                                <td> 
-                                    
-                                <span class="badge bg-danger"><a href="{{  route('employee.delete',$items->id)}}" class="m-3 p-lg-5"><h5>Delete</h5></a></span>
+                                <td>
+
+                                <span class="badge bg-danger"><a href="{{  route('employee.delete')}}" class="m-3 p-lg-5"><h5>Delete</h5></a></span>
                                 </td>
-                                
+
                             </tr>
-                            @endforeach
+                            {{-- @endforeach --}}
                         </tbody>
 
                     </table>
                 </div>
-                    
+
 
                 <div class="card-footer">
-                    
+
                 </div>
-             
+
 
             <!-- /.row -->
         </div><!-- /.container-fluid -->
